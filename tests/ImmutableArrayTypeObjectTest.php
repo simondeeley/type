dda3af82494cfee033285a01e723ef03b5a08bea
unwarysheep <unwarysheep@icloud.com>
2017-11-29 19:48:33 +0000

    Simplify tests

--- tests/ImmutableArrayTypeObjectTest.php
+++ tests/ImmutableArrayTypeObjectTest.php
@@ -11,102 +11,45 @@ declare(strict_types=1);
 namespace simondeeley\Tests;
 
 use RuntimeException;
-use ReflectionClass;
 use PHPUnit\Framework\TestCase;
-use simondeeley\Type\ImmutableType;
 use simondeeley\ImmutableArrayTypeObject;
-use simondeeley\Helpers\ImmutableArrayHelperMethods;
-use simondeeley\Helpers\ImmutableObjectHelperMethods;
-use simondeeley\Tests\TestCaseHelperMethods;
 
 /**
  * Test ImmutableArrayTypeObject
  *
  * @author Simon Deeley <s.deeley@icloud.com>
- *
  * @uses TestCase
- * @uses TestCaseHelperMethods
  */
 final class ImmutableArrayTypeObjectTest extends TestCase
 {
-    use TestCaseHelperMethods;
-
     /**
-     * Test interface implementations
+     * Test ImmutableObject prevents setting offsets
      *
      * @test
-     * @return void
-     */
-    final public function classShouldImplementInterfaces(): void
-    {
-        $type = new ReflectionClass(ImmutableArrayTypeObject::class);
-
-        $this->assertTrue($type->implementsInterface(ImmutableType::class));
-        $this->assertTrue($type->implementsInterface(\ArrayAccess::class));
-    }
-
-    /**
-     * Test class has methods
-     *
-     * @test
-     * @dataProvider allMethods
-     * @param string $method - Method name
-     * @param array $parameter - The paramaters to pass
-     * @return void
-     */
-    final public function classShouldHaveCorrectMethods(string $method, array $parameters): void
-    {
-        $type = new ReflectionClass(ImmutableArrayTypeObject::class);
-
-        $this->assertTrue($type->hasMethod($method));
-    }
-
-    /**
-     * Test exceptions are thrown
-     *
-     * @test
-     * @dataProvider implementedMethods
-     * @expectedException \RuntimeException
-     * @param string $method - Method name
-     * @param array $parameters - The paramaters to pass
-     * @return void
-     */
-    final public function shouldThrowAnExceptions(string $method, array $parameters): void
-    {
-        $type = $this->getMockForAbstractClass(ImmutableArrayTypeObject::class);
-
-        call_user_func_array(array($type, $method), $parameters);
-    }
-
-    /**
-     * Return methods that should be present
-     *
-     * @see https://phpunit.de/manual/current/en/writing-tests-for-phpunit.html#writing-tests-for-phpunit.data-providers
-     *
+     * @expectedException RuntimeException
      * @final
-     * @return array
+     * @return void
      */
-    final public function allMethods(): array
+    final public function shouldThrowExceptionWhenOffsetSetIsCalled(): void
     {
-        return array_merge(
-            $this->getDataProviderArray(ImmutableArrayTypeObject::class),
-            $this->getDataProviderArray(\ArrayAccess::class)
-        );
+        $object = $this->getMockForAbstractClass(ImmutableArrayTypeObject::class);
+
+        $object['foo'] = 'test';
     }
 
     /**
-     * Returns methods that should throw exception
+     * Test ImmutableObject prevents calling offsetUnset
      *
-     * @see https://phpunit.de/manual/current/en/writing-tests-for-phpunit.html#writing-tests-for-phpunit.data-providers
      *
+     * @test
+     * @expectedException RuntimeException
      * @final
-     * @return array
+     * @return void
      */
-    final public function implementedMethods(): array
+    final public function shouldThrowExceptionWhenOffsetUnsetIsCalled(): void
     {
-        return array_merge(
-            $this->getDataProviderArray(ImmutableArrayHelperMethods::class),
-            $this->getDataProviderArray(ImmutableObjectHelperMethods::class)
-        );
+        $object = $this->getMockForAbstractClass(ImmutableArrayTypeObject::class);
+
+        unset($object['foo']);
     }
 }
